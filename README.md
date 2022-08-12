b29dd778993b07fa814a9cd648822ef551a7e0c9
ronballard <ron.ballard@accenture.com>
2022-08-12 04:32:07 -0700

    module 1.0 initial install

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ca-api-sk1
+
+API for JS Dev Env Demo in Crypto Agility
+
+Automated deployment to heroku
---
+++ index.js
@@ -0,0 +1,26 @@
+var express =  require('express');
+var cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('SecOps')
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Kellyn","lastName":"Beck","email":"kellyn.beck@accenture.com"},
+    {"id": 2,"firstName":"Carl","lastName":"Dukatz","email":"carl.dukatz@accenture.com"},
+    {"id": 3,"firstName":"Stephen","lastName":"Harper","email":"stephen.harper@accenture.com"}
+    {"id": 4,"firstName":"Andrew","lastName":"Driscoll","email":"andrew.driscoll@accenture.com"}
+    {"id": 5,"firstName":"Ron","lastName":"Ballard","email":"ron.ballard@accenture.com"}
+  ]);
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port')); 
+});
